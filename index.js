7885c2543d2445c4ebdbff607173a0bf41ffe9fb
a.makihara <a.makihara@auris.jp>
2023-02-08 17:59:43 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const asana = require('asana');
 const core = require('@actions/core');
+const github = require('@actions/github')
 
-// console.log("asana-secret: " + core.getInput('asana-secret'));
-// const ASANA_SECRET = core.getInput('asana-secret')
-const ASANA_SECRET = 000000000 
+console.log("asana-secret: " + core.getInput('asana-secret'));
+const ASANA_SECRET = core.getInput('asana-secret')
+
 const client = asana.Client.create().useAccessToken(ASANA_SECRET);
 client.users.me().then(function(me) {
   console.log(me);
